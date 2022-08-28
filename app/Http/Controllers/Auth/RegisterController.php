04292c2ee3e4832725f2e50389fc25fabd078ff7
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-08-27 20:15:48 -0500

    Se desactivó el autologin

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,8 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
@@ -70,5 +72,13 @@ class RegisterController extends Controller
             'tipo' => $data['tipo'],
             'password' => Hash::make($data['password']),
         ]);
+    }
+    public function register(Request $request)
+    {
+        $this->validator($request->all())->validate();
+        event(new Registered($user = $this->create($request->all())));
+        return $this->registered($request, $user)
+           // ?: redirect($this->redirectPath());
+          ?: redirect()->route('home')->with('success', 'You are successfully Registered!');
     }
 }
