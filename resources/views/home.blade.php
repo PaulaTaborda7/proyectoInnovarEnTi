b8dab0f158b8cf07186734aed374893bdcc54161
ngiraldogi <ngiraldogi@unal.edu.co>
2022-08-30 15:29:18 -0500

    Se organizan algunos detalles de las vistas de administrador, agregando botones de regresar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,22 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Opciones del administrador') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Registrar Docente</a>
+                    <div class="container">
+                        <div class="justify-content-center">
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline"><button class="btn btn-outline-info">Registrar docente</button></a>
+                        </div>
+                    </div>
                     @endif
+                    <br>
                     <div class="container">
                         <div class="justify-content-center">
                             <a href="/materias" class="ml-4 text-sm text-color: primary"><button class="btn btn-outline-info">Administrar materia</button></a>
--- resources/views/materia/index.blade.php
+++ resources/views/materia/index.blade.php
@@ -1,87 +1,93 @@
 @extends('layouts.app')
 
 @section('template_title')
-    Materia
+Materia
 @endsection
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-sm-12">
-                <div class="card">
-                    <div class="card-header">
-                        <div style="display: flex; justify-content: space-between; align-items: center;">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <div style="display: flex; justify-content: space-between; align-items: center;">
 
-                            <span id="card_title">
-                                {{ __('Materia') }}
-                            </span>
+                        <span id="card_title">
+                            {{ __('Materia') }}
+                        </span>
 
-                             <div class="float-right">
-                                <a href="{{ route('materias.create') }}" class="btn btn-primary btn-sm float-right"  data-placement="left">
-                                  {{ __('Create New') }}
-                                </a>
-                              </div>
-                        </div>
-                    </div>
-                    @if ($message = Session::get('success'))
-                        <div class="alert alert-success">
-                            <p>{{ $message }}</p>
-                        </div>
-                    @endif
-                    <br>
-                    <div class="d-md-flex justify-content-md-end">
-                        <form action="{{ route('materias.index') }}" method="GET">
-                            <div class="btn-group">
-                                <input type="text" name="busqueda" class="form-control">
-                                <input type="submit" value="enviar" class="btn btn-primary">
-                            </div>
-                        </form>
-                    </div>
-                    
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        
-										<th>Matnombremateria</th>
-										<th>Matdescripcion</th>
-
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($materias as $materia)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            
-											<td>{{ $materia->matNombreMateria }}</td>
-											<td>{{ $materia->matDescripcion }}</td>
-
-                                            <td>
-                                                <form action="{{ route('materias.destroy',$materia->id) }}" method="POST">
-                                                    <a class="btn btn-sm btn-primary " href="{{ route('materias.show',$materia->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('materias.edit',$materia->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Delete</button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <tfoot>
-                                <tr>
-                                    <td colspan="4">{{$materias->appends(['busqueda'=>$busqueda])}}</td>
-                                </tr>
-                            </tfoot>
+                        <div class="float-right">
+                            <a href="{{ route('materias.create') }}" class="btn btn-primary btn-sm float-right" data-placement="left">
+                                {{ __('Create New') }}
+                            </a>
                         </div>
                     </div>
                 </div>
-                {{-- {!! $materias->links() !!} --}}
+                @if ($message = Session::get('success'))
+                <div class="alert alert-success">
+                    <p>{{ $message }}</p>
+                </div>
+                @endif
+                <br>
+                <div class="d-md-flex justify-content-md-end">
+                    <form action="{{ route('materias.index') }}" method="GET">
+                        <div class="btn-group">
+                            <input type="text" name="busqueda" class="form-control">
+                            <input type="submit" value="enviar" class="btn btn-primary">
+                        </div>
+                    </form>
+                </div>
+
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead class="thead">
+                                <tr>
+                                    <th>No</th>
+
+                                    <th>Matnombremateria</th>
+                                    <th>Matdescripcion</th>
+
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($materias as $materia)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+
+                                    <td>{{ $materia->matNombreMateria }}</td>
+                                    <td>{{ $materia->matDescripcion }}</td>
+
+                                    <td>
+                                        <form action="{{ route('materias.destroy',$materia->id) }}" method="POST">
+                                            <a class="btn btn-sm btn-primary " href="{{ route('materias.show',$materia->id) }}"><i class="fa fa-fw fa-eye"></i> Show</a>
+                                            <a class="btn btn-sm btn-success" href="{{ route('materias.edit',$materia->id) }}"><i class="fa fa-fw fa-edit"></i> Edit</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i> Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <tfoot>
+                            <tr>
+                                <td colspan="4">{{$materias->appends(['busqueda'=>$busqueda])}}</td>
+                            </tr>
+                        </tfoot>
+                        <div class="container">
+                            <div class="justify-content-center">
+                                <a href="/home" class="ml-4 text-sm text-color: primary"><button class="btn btn-outline-info">Regresar</button></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+            {{-- {!! $materias->links() !!} --}}
         </div>
     </div>
+</div>
+
 @endsection
