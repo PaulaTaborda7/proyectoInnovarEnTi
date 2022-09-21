5eecc5aec2840f7732afdec87d8c08199f4c8b74
ngiraldogi <ngiraldogi@unal.edu.co>
2022-09-21 10:33:09 -0500

    Se modifica la vista de consulta de docentes

--- resources/views/consultaDocentes.blade.php
+++ resources/views/consultaDocentes.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Esta es la lista de docentes pertenecientes a esta institución') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,8 +13,35 @@
                         {{ session('status') }}
                     </div>
                     @endif
-
-                    {{ __('Esta es la lista de docentes pertenecientes a esta institución') }}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
