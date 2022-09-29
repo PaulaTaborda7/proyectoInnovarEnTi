4c24ed75173d6c62178a2334aab2a0c53c20b390
ngiraldogi <ngiraldogi@unal.edu.co>
2022-09-29 18:33:49 -0500

    Creación del catálogo de REDs habilitados para el estudiante, así como la prueba de ellos

--- resources/views/estudiante/catalogoRecursosHabilitados.blade.php
+++ resources/views/estudiante/catalogoRecursosHabilitados.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="col-auto p-5 text-center">
-        <h1><strong>Catálogo de Recursos Educativos Digitales : COMPLETO</strong> </h1>
+        <h1><strong>Catálogo de Recursos Educativos Digitales habilitados</strong> </h1>
     </div>
 </div>
 <div class="container" style="margin-top: 10px">
@@ -11,7 +11,7 @@
         <div class="col-lg-12">
             <hr>
             <div class="row">
-                @foreach($recursos as $recurso)
+                @foreach($reds_habilitados as $recurso)
                 <div class="col-lg-3">
                     <div class="card text-center"  style="margin-bottom: 20px; height: auto;">
                         <br>
@@ -23,12 +23,7 @@
                         </div>
                         <div class="card-footer" class="justify-content-center">
                             <div>
-                                <a class="btn btn-sm btn-info" href="{{ route('recurso.educativo.digital.Docente',$recurso->id) }}"><i class="fa fa-eye"></i> Probar RED</a>
-                            </div>
-                            <br>
-                            <div>
-                                <a class="btn btn-sm btn-success" href="#"><i class="fa fa-fw fa-edit"></i>Habilitar</a>
-                                <a class="btn btn-danger btn-sm" href="#"><i class="fa fa-fw fa-trash"></i>Desahibilitar</a>
+                                <a class="btn btn-sm btn-info" href="{{ route('recurso.educativo.digital.Estudiante',$recurso->id) }}"><i class="fa fa-eye"></i> Probar RED</a>
                             </div>
                         </div>
                     </div>
@@ -43,7 +38,7 @@
         <div class="col-lg-12">
             <hr>
             <div  class="col-auto p-5 text-center">
-                <a class="ml-4 text-sm btn btn-primary" href="/vistadocentes">Regresar</a>
+                <a class="ml-4 text-sm btn btn-primary" href="/vistaestudiantes">Regresar</a>
             </div>
         </div>
     </div>
